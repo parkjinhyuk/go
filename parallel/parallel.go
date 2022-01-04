e86690f536580598b9fc728e4ddb13a0e2445140
parkjinhyuk <jinhyuk8007@gmail.com>
2022-01-04 16:28:11 +0900

    parallel

---
+++ parallel/parallel.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func Min(a []int) int {
+	if len(a) == 0 {
+		return 0
+	}
+	min := a[0]
+	for _, e := range a[1:] {
+		if min > e {
+			min = e
+		}
+	}
+	return min
+}
+
+func ParallelMin(a []int, n int) int {
+	if len(a) < n {
+		return Min(a)
+	}
+
+	mins := make([]int, n)
+	size := (len(a) + n - 1) / n
+	var wg sync.WaitGroup
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			begin, end := i*size, (i+1)*size
+			if end > len(a) {
+				end = len(a)
+			}
+			mins[i] = Min(a[begin:end])
+		}(i)
+	}
+	wg.Wait()
+	return Min(mins)
+}
+
+func main() {
+	fmt.Println(ParallelMin([]int{
+		83, 46, 49, 23, 92,
+		48, 39, 91, 44, 99,
+		25, 42, 74, 56, 23,
+	}, 5))
+}
